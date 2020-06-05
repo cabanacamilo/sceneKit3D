9c00c56bf0b6d33b9e0a2298a5c575aada33fe30
camilo cabana <camilo_11_04@hotmail.com>
2020-06-05 18:34:31 +0900

    3d animation AR

--- sceneKit3D/ViewController.swift
+++ sceneKit3D/ViewController.swift
@@ -13,6 +13,8 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var animations = [String: CAAnimation]()
+    var punch = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +26,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         // Set the scene to the view
         sceneView.scene = scene
+        
+        loadAnimations()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -72,4 +77,76 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    
+    func loadAnimations () {
+        // Load the character in the idle animation
+        let idleScene = SCNScene(named: "art.scnassets/avatar/Macarena.dae")!
+        
+        // This node will be parent of all the animation models
+        let node = SCNNode()
+        
+        // Add all the child nodes to the parent node
+        for child in idleScene.rootNode.childNodes {
+            node.addChildNode(child)
+        }
+        
+        // Set up some properties
+//        node.position = SCNVector3(0, -1, -2)
+        node.position = SCNVector3(0, -2, -2.5)
+        node.scale = SCNVector3(0.2, 0.2, 0.2)
+        
+        // Add the node to the scene
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        // Load all the DAE animations
+//        loadAnimation(withKey: "dancing", sceneName: "art.scnassets/avatar/Punching.dae", animationIdentifier: "Punching.dae")
+    }
+    
+//    func loadAnimation(withKey: String, sceneName:String, animationIdentifier:String) {
+//        guard let sceneURL = Bundle.main.url(forResource: sceneName, withExtension: "dae") else { return }
+//        let sceneSource = SCNSceneSource(url: sceneURL, options: nil)
+//
+//        if let animationObject = sceneSource?.entryWithIdentifier(animationIdentifier, withClass: CAAnimation.self) {
+//            // The animation will only play once
+//            animationObject.repeatCount = 1
+//            // To create smooth transitions between animations
+//            animationObject.fadeInDuration = CGFloat(1)
+//            animationObject.fadeOutDuration = CGFloat(0.5)
+//
+//            // Store the animation for later use
+//            animations[withKey] = animationObject
+//        }
+//    }
+    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        let location = touches.first!.location(in: sceneView)
+//
+//        // Let's test if a 3D Object was touch
+//        var hitTestOptions = [SCNHitTestOption: Any]()
+//        hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+//
+//        let hitResults: [SCNHitTestResult]  = sceneView.hitTest(location, options: hitTestOptions)
+//        print(hitResults)
+//
+//        if hitResults.first != nil {
+//            if punch {
+//                playAnimation(key: "dancing")
+//            } else {
+//                stopAnimation(key: "dancing")
+//            }
+//            punch = !punch
+//            return
+//        }
+//    }
+    
+//    func playAnimation(key: String) {
+//        // Add the animation to start playing it right away
+//        guard let animationKey = animations[key] else { return }
+//        sceneView.scene.rootNode.addAnimation(animationKey, forKey: key)
+//    }
+//    
+//    func stopAnimation(key: String) {
+//        // Stop the animation with a smooth transition
+//        sceneView.scene.rootNode.removeAnimation(forKey: key, blendOutDuration: CGFloat(0.5))
+//    }
 }
